7a8a1e4010a7b0bbf8f88fcfb2a5c2da8d2655be
Joaquin Menchaca <suavecali@yahoo.com>
2022-05-17 14:29:16 -0700

    remove provider constraints

--- provider.tf
+++ provider.tf
@@ -1,20 +1,8 @@
 provider "aws" {
-  version = ">= 2.43.0"
   region  = var.region
 }
 
-provider "random" {
-  version = "~> 2.2.1"
-}
-
-provider "local" {
-  version = "~> 1.4.0"
-}
-
-provider "null" {
-  version = "~> 2.1.2"
-}
-
-provider "template" {
-  version = "~> 2.1.2"
-}
+provider "random" {}
+provider "local" {}
+provider "null" {}
+provider "template" {}
