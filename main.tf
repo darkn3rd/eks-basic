3c2cb8e49117ce694150b1d52eac05e5c9302a19
joaquin <joaquin@dgraph.io>
2022-05-17 15:38:39 -0700

    add explicit cluster_version otherwise search errors with ami

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
+  version = "3.2.0"
 
   name                 = "ekstf-vpc"
-  cidr                 = "192.168.0.0/16"
+  cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["192.168.160.0/19", "192.168.128.0/19", "192.168.96.0/19"]
-  public_subnets       = ["192.168.64.0/19", "192.168.32.0/19", "192.168.0.0/19"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -38,11 +38,12 @@ module "vpc" {
 }
 
 module "eks" {
-  source       = "terraform-aws-modules/eks/aws"
-  version      = "18.21.0"
-  cluster_name = var.eks_cluster_name
-  subnets      = module.vpc.private_subnets
-  vpc_id       = module.vpc.vpc_id
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+  cluster_name    = var.eks_cluster_name
+  cluster_version = "1.22"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   workers_additional_policies = var.workers_additional_policies
 
@@ -53,4 +54,12 @@ module "eks" {
       asg_desired_capacity = 2
     }
   ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
